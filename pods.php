023151b3a06d79df63b5f79219c8ebd4a6cd73c3
maiki <maiki@interi.org>
2013-11-19 00:33:39 -0800

    Updated pods.php
    Added sidebar like blog posts, tightens up the layout a bit more.

--- pods.php
+++ pods.php
@@ -5,27 +5,30 @@ Template Name: Pods
 
 get_header(); ?>
 
-<?php if ( function_exists( 'breadcrumb_trail' ) ) breadcrumb_trail(); ?>
-
 <?php echo GeoMashup::map('height=200&zoom=15') ?>
 
-<?php while(have_posts()): the_post(); ?>
-
-	<div id="page" class="fix">
-		<div id="page-inner" class="container fix">
-			<div id="content-part">
-				<article id="entry-<?php the_ID(); ?>" <?php post_class('entry'); ?>>
+<section class="section <?php echo oxy_get_option('blog_swatch'); ?>">
+    <div class="container">
+        <div class="row-fluid">
+            <?php if( oxy_get_option('blog_layout') == 'sidebar-left' ): ?>
+            <aside class="span3 sidebar">
+                <?php get_sidebar(); ?>
+            </aside>
+            <?php endif; ?>
+            <div class="<?php echo oxy_get_option('blog_layout') == 'full-width' ? 'span12':'span9' ; ?>">
+                <?php while ( have_posts() ) : the_post(); ?>
 
 					<?php pods_content() ?>
 
-					</article>
-					</div><!--/content-part-->
-
-					<?php endwhile; ?>
-				<div id="sidebar" class="sidebar-right">
-				<?php get_sidebar(); ?>
-			</div><!--/sidebar-->
-		</div><!--/page-inner-->
-	</div><!--/page-->
+                <?php endwhile; ?>
+            </div>
+            <?php if( oxy_get_option('blog_layout') == 'sidebar-right' ): ?>
+            <aside class="span3 sidebar">
+                <?php get_sidebar(); ?>
+            </aside>
+            <?php endif; ?>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
